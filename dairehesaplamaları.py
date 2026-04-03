41da0cc52ecd4296d8704c401513491059edb473
Yusuf Özdemir <yusufzdemr2@gmail.com>
2026-04-03 21:33:51 +0300

    Add files via upload

---
+++ dairehesaplamaları.py
@@ -0,0 +1,5 @@
+x = float(input("Dairenin yarıçapını giriniz: "))
+daire_alani = 3.14 * x ** 2
+daire_cevresi = 2 * 3.14 * x
+print("Dairenin alanı:", daire_alani)
+print("Dairenin çevresi:", daire_cevresi)
